964418af5ce0d1a8e1dd5924fa9111efe3862304
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2024-04-16 08:49:05 +0200

    Revert "Merge branch 'main' into apex-121-bc-scm"
    This reverts commit 7585f3b62a7bd240e033e496ad0680c96eec67f3, reversing
    changes made to 146d022c19b164e50286a8776a47acaad6134bc0.

--- contracts/interfaces/IBridgeContractStructs.sol
+++ contracts/interfaces/IBridgeContractStructs.sol
@@ -31,7 +31,6 @@ interface IBridgeContractStructs {
         string observedTransactionHash;
         uint256 nonce;
         uint256 blockHeight;
-        string sourceChainID;
         Receiver[] receivers;
     }
 
