302173c2cb9702e77be095de427609cf644ef2d4
MiroslavStefanovic <mstef@uns.ac.rs>
2024-04-19 09:52:19 +0200

    changing deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const bridge = await ethers.deployContract("Bridge");
+  const claimsHelper = await ethers.deployContract("ClaimsHelper");
+  const claims = await ethers.deployContract("Claims");
+  const signedBatchManager = await ethers.deployContract("SignedBatchManager");
+  const slotsManager = await ethers.deployContract("SlotsManager");
+  const utxosManager = await ethers.deployContract("UTOXsManager");
+  const validatorsContract = await ethers.deployContract("ValidatorsContract");
 
-  const bridgeContract = await ethers.deployContract("BridgeContract");
-
-  await bridgeContract.waitForDeployment();
-
-  console.log(
-    `BridgeContract deployed to ${bridgeContract.target}`
-  );
+  console.log(`Bridge deployed to ${bridge.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
