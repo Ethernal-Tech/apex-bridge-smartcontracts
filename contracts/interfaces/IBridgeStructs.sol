9202cab9d99f9e53b0175d6eeba16a1432683c7c
Igor Crevar <crewce@gmail.com>
2024-05-14 11:40:52 +0200

    signedBatch optimization a little bit (#30)

--- contracts/interfaces/IBridgeStructs.sol
+++ contracts/interfaces/IBridgeStructs.sol
@@ -20,6 +20,11 @@ interface IBridgeStructs {
         UTXOs usedUTXOs;
     }
 
+    struct ConfirmedSignedBatchData {
+        uint256[] includedTransactions;
+        UTXOs usedUTXOs;
+    }
+
     struct ConfirmedBatch {
         uint256 id;
         string rawTransaction;
