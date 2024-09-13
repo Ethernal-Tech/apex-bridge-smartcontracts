346a49598d3b33501b765e895ecd8be54551a980
MiroslavStefanovic <mstef@uns.ac.rs>
2024-09-13 11:48:31 +0200

    addidng network reference

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
+// import "dotenv/config";
 
 const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+  },
+};
+
+module.exports = {
+  networks: {
+    hardhat: {},
+    // blade: {
+    //   url: process.env.BLADE_URL,
+    //   accounts: [process.env.BLADE_PRIVATE_KEY],
+    //   gasPrice: 35000000000,
+    // },
+  },
   solidity: {
     version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000,
+        runs: 100,
       },
     },
   },
