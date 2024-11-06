ca213d438ed0e13e2b13290b520a25e313623a22
MiroslavStefanovic <mstef@uns.ac.rs>
2024-11-06 19:19:03 +0100

    fix

--- contracts/Claims.sol
+++ contracts/Claims.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "./interfaces/IBridgeStructs.sol";
 import "./Bridge.sol";
-import "./Bridge.sol";
 import "./ClaimsHelper.sol";
 import "./Validators.sol";
 
