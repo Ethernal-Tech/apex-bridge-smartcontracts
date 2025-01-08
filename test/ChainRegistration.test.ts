6fe545ff8304666bc918f76c914c7580ed0a4819
MiroslavStefanovic <mstef@uns.ac.rs>
2025-01-08 10:26:10 +0100

    update registered chain by owner

--- test/ChainRegistration.test.ts
+++ test/ChainRegistration.test.ts
@@ -48,12 +48,13 @@ describe("Chain Registration", function () {
 
       expect(await claims.isChainRegistered(chain1.id)).to.be.false;
 
-      await bridge.connect(owner).registerChain(chain1, 100, validatorsCardanoData);
+      await bridge.connect(owner).registerChain(chain1, 100, 100, validatorsCardanoData);
 
       expect(await claims.isChainRegistered(chain1.id)).to.be.true;
       expect(await bridge.getAllRegisteredChains()).to.have.length(1);
       expect((await bridge.getAllRegisteredChains())[0].id).to.equal(1);
       expect(await claims.getChainTokenQuantity(chain1.id)).to.equal(100); //it should not be changed
+      expect(await claims.getChainWrappedTokenQuantity(chain1.id)).to.equal(100); //it should not be changed
       expect(await validatorsc.getValidatorsChainData(chain1.id)).to.have.length(5);
       expect((await validatorsc.getValidatorsChainData(chain1.id))[0].key[0]).to.equal(
         validatorsCardanoData[0].data.key[0]
@@ -61,12 +62,13 @@ describe("Chain Registration", function () {
 
       validatorsCardanoData[0].data.key[0] = BigInt(10);
 
-      await bridge.connect(owner).registerChain(chain1, 10, validatorsCardanoData);
+      await bridge.connect(owner).registerChain(chain1, 10, 10, validatorsCardanoData);
 
       expect(await claims.isChainRegistered(chain1.id)).to.be.true;
       expect(await bridge.getAllRegisteredChains()).to.have.length(1);
       expect((await bridge.getAllRegisteredChains())[0].id).to.equal(1);
       expect(await claims.getChainTokenQuantity(chain1.id)).to.equal(100); //it should not be changed
+      expect(await claims.getChainWrappedTokenQuantity(chain1.id)).to.equal(100); //it should not be changed
       expect(await validatorsc.getValidatorsChainData(chain1.id)).to.have.length(5);
       expect((await validatorsc.getValidatorsChainData(chain1.id))[0].key[0]).to.equal(
         validatorsCardanoData[0].data.key[0]
