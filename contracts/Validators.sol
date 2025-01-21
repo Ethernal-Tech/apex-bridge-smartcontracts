325ed59a5bd0b6b857fabe987ddbdf497afc0285
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2025-01-21 07:40:56 +0100

    revert with an error when accessing missing map values

--- contracts/Validators.sol
+++ contracts/Validators.sol
@@ -49,6 +49,8 @@ contract Validators is IBridgeStructs, Initializable, OwnableUpgradeable, UUPSUp
     }
 
     function getValidatorIndex(address _addr) public view returns (uint8) {
+        uint8 index = addressValidatorIndex[_addr];
+        if (index == 0) revert NotValidator();
         return addressValidatorIndex[_addr];
     }
 
