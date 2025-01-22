93d220fee4658d4a524ab7c91c914a97c71c75ae
MiroslavStefanovic <mstef@uns.ac.rs>
2025-01-22 14:00:10 +0100

    additional fix

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -151,7 +151,7 @@ contract Bridge is IBridge, Initializable, OwnableUpgradeable, UUPSUpgradeable {
         bytes32 messageHashBytes32 = keccak256(abi.encodePacked("Hello world of apex-bridge:", msg.sender));
 
         if (_chainType == 0) {
-            bytes memory messageHashBytes = bytes32ToBytesAssembly(messageHashBytes32);
+            bytes memory messageHashBytes = _bytes32ToBytesAssembly(messageHashBytes32);
             if (
                 !validators.isSignatureValid(messageHashBytes, _keySignature, _validatorChainData.key[0], false) ||
                 !validators.isSignatureValid(messageHashBytes, _keyFeeSignature, _validatorChainData.key[1], false)
@@ -243,7 +243,7 @@ contract Bridge is IBridge, Initializable, OwnableUpgradeable, UUPSUpgradeable {
         return claims.getBatchTransactions(_chainId, _batchId);
     }
 
-    function bytes32ToBytesAssembly(bytes32 input) public pure returns (bytes memory) {
+    function _bytes32ToBytesAssembly(bytes32 input) internal pure returns (bytes memory) {
         bytes memory output = new bytes(32);
 
         assembly {
