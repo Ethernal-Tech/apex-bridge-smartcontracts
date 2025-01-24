66cae88679742c83a7170318c55e7f8e2773056b
Nikola Jovićević <124697314+nikola-j265@users.noreply.github.com>
2025-01-24 17:49:35 +0100

    audit: APEX-453 Duplicate access to storage variable chainTokenQuantity (#80)
    ---------
    
    Co-authored-by: Igor Crevar <crewce@gmail.com>

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -32,12 +32,7 @@ contract Admin is IBridgeStructs, Initializable, OwnableUpgradeable, UUPSUpgrade
     }
 
     function updateChainTokenQuantity(uint8 _chainId, bool _isIncrease, uint256 _quantity) external onlyFundAdmin {
-        if (!claims.isChainRegistered(_chainId)) revert ChainIsNotRegistered(_chainId);
-        if (!_isIncrease && claims.chainTokenQuantity(_chainId) < _quantity)
-            revert NegativeChainTokenAmount(claims.chainTokenQuantity(_chainId), _quantity);
-
         claims.updateChainTokenQuantity(_chainId, _isIncrease, _quantity);
-
         emit UpdatedChainTokenQuantity(_chainId, _isIncrease, _quantity);
     }
 
