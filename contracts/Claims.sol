c045b15780de1bf3b1a7a39b608386f35682032a
MiroslavStefanovic <mstef@uns.ac.rs>
2025-04-28 15:00:52 +0200

    renaming

--- contracts/Claims.sol
+++ contracts/Claims.sol
@@ -148,12 +148,12 @@ contract Claims is IBridgeStructs, Initializable, OwnableUpgradeable, UUPSUpgrad
         uint8 _destinationChainId = _claim.destinationChainId;
 
         if (chainTokenQuantity[_destinationChainId] < _nativeCurrencyAmountDestination) {
-            emit NotEnoughFunds("BRC - Native Currency", i, chainTokenQuantity[_destinationChainId]);
+            emit NotEnoughFunds("BRC - Currency", i, chainTokenQuantity[_destinationChainId]);
             return;
         }
 
         if (chainWrappedTokenQuantity[_destinationChainId] < _wrappedTokenAmountDestination) {
-            emit NotEnoughFunds("BRC - Wrapped Token", i, chainWrappedTokenQuantity[_destinationChainId]);
+            emit NotEnoughFunds("BRC - Token", i, chainWrappedTokenQuantity[_destinationChainId]);
             return;
         }
 
@@ -263,12 +263,12 @@ contract Claims is IBridgeStructs, Initializable, OwnableUpgradeable, UUPSUpgrad
 
         if (_claim.shouldDecrementHotWallet && _claim.retryCounter == 0) {
             if (chainTokenQuantity[originChainId] < _claim.originAmount) {
-                emit NotEnoughFunds("RRC - Native Currency", 0, chainTokenQuantity[originChainId]);
+                emit NotEnoughFunds("RRC - Currency", 0, chainTokenQuantity[originChainId]);
                 return;
             }
 
             if (chainWrappedTokenQuantity[originChainId] < _claim.originWrappedAmount) {
-                emit NotEnoughFunds("RRC - Wrapped Token", 0, chainWrappedTokenQuantity[originChainId]);
+                emit NotEnoughFunds("RRC - Token", 0, chainWrappedTokenQuantity[originChainId]);
                 return;
             }
         }
