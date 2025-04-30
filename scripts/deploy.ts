7873ac9ffc2142b6216cb3b52f935f6cc6866296
MiroslavStefanovic <mstef@uns.ac.rs>
2025-04-30 11:29:02 +0200

    testing

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,6 +3,8 @@ import { ethers } from "hardhat";
 async function main() {
   const config = require("./config.json");
 
+  const owner = await ethers.getSigners();
+
   //deployment of contract logic
   const Bridge = await ethers.getContractFactory("Bridge");
   const bridgeLogic = await Bridge.deploy();
@@ -32,150 +34,150 @@ async function main() {
 
   const bridgeProxy = await BridgeProxy.deploy(
     await bridgeLogic.getAddress(),
-    Bridge.interface.encodeFunctionData("initialize", [])
+    Bridge.interface.encodeFunctionData("initialize", [owner])
   );
 
-  const claimsHelperProxy = await ClaimsHelperProxy.deploy(
-    await claimsHelperLogic.getAddress(),
-    ClaimsHelper.interface.encodeFunctionData("initialize", [])
-  );
+  // const claimsHelperProxy = await ClaimsHelperProxy.deploy(
+  //   await claimsHelperLogic.getAddress(),
+  //   ClaimsHelper.interface.encodeFunctionData("initialize", [owner])
+  // );
 
-  const claimsProxy = await ClaimsProxy.deploy(
-    await claimsLogic.getAddress(),
-    Claims.interface.encodeFunctionData("initialize", [2, 5])
-  );
+  // const claimsProxy = await ClaimsProxy.deploy(
+  //   await claimsLogic.getAddress(),
+  //   Claims.interface.encodeFunctionData("initialize", [owner, 2, 5])
+  // );
 
-  const signedBatchesProxy = await SignedBatchesProxy.deploy(
-    await signedBatchesLogic.getAddress(),
-    SignedBatches.interface.encodeFunctionData("initialize", [])
-  );
+  // const signedBatchesProxy = await SignedBatchesProxy.deploy(
+  //   await signedBatchesLogic.getAddress(),
+  //   SignedBatches.interface.encodeFunctionData("initialize", [owner])
+  // );
 
-  const slotsProxy = await SlotsProxy.deploy(
-    await slotsLogic.getAddress(),
-    Slots.interface.encodeFunctionData("initialize", [])
-  );
+  // const slotsProxy = await SlotsProxy.deploy(
+  //   await slotsLogic.getAddress(),
+  //   Slots.interface.encodeFunctionData("initialize", [owner])
+  // );
 
-  const validatorsAddresses = [
-    config.Validators.validator1address,
-    config.Validators.validator2address,
-    config.Validators.validator3address,
-    config.Validators.validator4address,
-    config.Validators.validator5address,
-  ];
+  // const validatorsAddresses = [
+  //   config.Validators.validator1address,
+  //   config.Validators.validator2address,
+  //   config.Validators.validator3address,
+  //   config.Validators.validator4address,
+  //   config.Validators.validator5address,
+  // ];
 
-  const validatorsProxy = await ValidatorsProxy.deploy(
-    await validatorscLogic.getAddress(),
-    Validators.interface.encodeFunctionData("initialize", [validatorsAddresses])
-  );
+  // const validatorsProxy = await ValidatorsProxy.deploy(
+  //   await validatorscLogic.getAddress(),
+  //   Validators.interface.encodeFunctionData("initialize", [owner, validatorsAddresses])
+  // );
 
-  //casting proxy contracts to contract logic
-  const BridgeDeployed = await ethers.getContractFactory("Bridge");
-  const bridge = BridgeDeployed.attach(bridgeProxy.target);
+  // //casting proxy contracts to contract logic
+  // const BridgeDeployed = await ethers.getContractFactory("Bridge");
+  // const bridge = BridgeDeployed.attach(bridgeProxy.target);
 
-  const ClaimsHelperDeployed = await ethers.getContractFactory("ClaimsHelper");
-  const claimsHelper = ClaimsHelperDeployed.attach(claimsHelperProxy.target);
+  // const ClaimsHelperDeployed = await ethers.getContractFactory("ClaimsHelper");
+  // const claimsHelper = ClaimsHelperDeployed.attach(claimsHelperProxy.target);
 
-  const ClaimsDeployed = await ethers.getContractFactory("Claims");
-  const claims = ClaimsDeployed.attach(claimsProxy.target);
+  // const ClaimsDeployed = await ethers.getContractFactory("Claims");
+  // const claims = ClaimsDeployed.attach(claimsProxy.target);
 
-  const SignedBatchesDeployed = await ethers.getContractFactory("SignedBatches");
-  const signedBatches = SignedBatchesDeployed.attach(signedBatchesProxy.target);
+  // const SignedBatchesDeployed = await ethers.getContractFactory("SignedBatches");
+  // const signedBatches = SignedBatchesDeployed.attach(signedBatchesProxy.target);
 
-  const SlotsDeployed = await ethers.getContractFactory("Slots");
-  const slots = SlotsDeployed.attach(slotsProxy.target);
+  // const SlotsDeployed = await ethers.getContractFactory("Slots");
+  // const slots = SlotsDeployed.attach(slotsProxy.target);
 
-  const ValidatorscDeployed = await ethers.getContractFactory("Validators");
-  const validatorsc = ValidatorscDeployed.attach(validatorsProxy.target);
+  // const ValidatorscDeployed = await ethers.getContractFactory("Validators");
+  // const validatorsc = ValidatorscDeployed.attach(validatorsProxy.target);
 
-  await bridge.setDependencies(
-    claimsProxy.target,
-    signedBatchesProxy.target,
-    slotsProxy.target,
-    validatorsProxy.target
-  );
+  // await bridge.setDependencies(
+  //   claimsProxy.target,
+  //   signedBatchesProxy.target,
+  //   slotsProxy.target,
+  //   validatorsProxy.target
+  // );
 
-  await claims.setDependencies(bridgeProxy.target, claimsHelper.target, validatorsProxy.target);
+  // await claims.setDependencies(bridgeProxy.target, claimsHelper.target, validatorsProxy.target);
 
-  await claimsHelper.setDependencies(claims.target, signedBatches.target);
+  // await claimsHelper.setDependencies(claims.target, signedBatches.target);
 
-  await signedBatches.setDependencies(bridge.target, claimsHelper.target, validatorsProxy.target);
+  // await signedBatches.setDependencies(bridge.target, claimsHelper.target, validatorsProxy.target);
 
-  await slots.setDependencies(bridge.target, validatorsProxy.target);
+  // await slots.setDependencies(bridge.target, validatorsProxy.target);
 
-  await validatorsc.setDependencies(bridge.target);
+  // await validatorsc.setDependencies(bridge.target);
 
-  const chain = {
-    id: config.Chain.id,
-    chainType: config.Chain.chainType,
-    addressMultisig: config.Chain.addressMultisig,
-    addressFeePayer: config.Chain.addressFeePayer,
-  };
+  // const chain = {
+  //   id: config.Chain.id,
+  //   chainType: config.Chain.chainType,
+  //   addressMultisig: config.Chain.addressMultisig,
+  //   addressFeePayer: config.Chain.addressFeePayer,
+  // };
 
-  const validatorsChainData = [
-    {
-      addr: config.Validators.validator1address,
-      data: {
-        key: config.Validators.validator1key,
-      },
-    },
-    {
-      addr: config.Validators.validator2address,
-      data: {
-        key: config.Validators.validator2key,
-      },
-    },
-    {
-      addr: config.Validators.validator3address,
-      data: {
-        key: config.Validators.validator3key,
-      },
-    },
-    {
-      addr: config.Validators.validator4address,
-      data: {
-        key: config.Validators.validator4key,
-      },
-    },
-    {
-      addr: config.Validators.validator5address,
-      data: {
-        key: config.Validators.validator5key,
-      },
-    },
-  ];
+  // const validatorsChainData = [
+  //   {
+  //     addr: config.Validators.validator1address,
+  //     data: {
+  //       key: config.Validators.validator1key,
+  //     },
+  //   },
+  //   {
+  //     addr: config.Validators.validator2address,
+  //     data: {
+  //       key: config.Validators.validator2key,
+  //     },
+  //   },
+  //   {
+  //     addr: config.Validators.validator3address,
+  //     data: {
+  //       key: config.Validators.validator3key,
+  //     },
+  //   },
+  //   {
+  //     addr: config.Validators.validator4address,
+  //     data: {
+  //       key: config.Validators.validator4key,
+  //     },
+  //   },
+  //   {
+  //     addr: config.Validators.validator5address,
+  //     data: {
+  //       key: config.Validators.validator5key,
+  //     },
+  //   },
+  // ];
 
-  await bridge.registerChain(chain, config.Chain.totalSupply, validatorsChainData);
+  // await bridge.registerChain(chain, config.Chain.totalSupply, validatorsChainData);
 
-  console.log("BridgeLogic deployed at:", bridgeLogic.target);
-  console.log("Bridge deployed at:", bridge.target);
-  console.log("---");
-  console.log("ClaimsLogic deployed at:", claimsLogic.target);
-  console.log("Claims deployed at:", claims.target);
-  console.log("---");
-  console.log("ClaimsHelperLogic deployed at:", claimsHelperLogic.target);
-  console.log("ClaimsHelper deployed at:", claimsHelper.target);
-  console.log("---");
-  console.log("SignedBatchesLogic deployed at:", signedBatchesLogic.target);
-  console.log("SignedBatches deployed at:", signedBatches.target);
-  console.log("---");
-  console.log("SlotsLogic deployed at:", slotsLogic.target);
-  console.log("Slots deployed at:", slots.target);
-  console.log("---");
-  console.log("ValidatorsLogic deployed at:", validatorscLogic.target);
-  console.log("Validators deployed at:", validatorsc.target);
-  console.log("---");
-  console.log("BridgeLogic owner:", await bridgeLogic.owner());
-  console.log("Bridge owner:", await bridge.owner());
-  console.log("---");
-  console.log("ClaimsLogic maxNumberOfTransactions:", await claimsLogic.maxNumberOfTransactions());
-  console.log("Claims maxNumberOfTransactions:", await claims.maxNumberOfTransactions());
-  console.log("ClaimsLogic timeoutBlocksNumber:", await claimsLogic.timeoutBlocksNumber());
-  console.log("Claims timeoutBlocksNumber:", await claims.timeoutBlocksNumber());
-  console.log("---");
-  console.log("ValdatorsLogic numberOfValidators:", await validatorscLogic.validatorsCount());
-  console.log("Valdators numberOfValidators:", await validatorsc.validatorsCount());
-  console.log("---");
-  console.log("Validators chain data", await bridge.getValidatorsChainData(chain.id));
+  // console.log("BridgeLogic deployed at:", bridgeLogic.target);
+  // console.log("Bridge deployed at:", bridge.target);
+  // console.log("---");
+  // console.log("ClaimsLogic deployed at:", claimsLogic.target);
+  // console.log("Claims deployed at:", claims.target);
+  // console.log("---");
+  // console.log("ClaimsHelperLogic deployed at:", claimsHelperLogic.target);
+  // console.log("ClaimsHelper deployed at:", claimsHelper.target);
+  // console.log("---");
+  // console.log("SignedBatchesLogic deployed at:", signedBatchesLogic.target);
+  // console.log("SignedBatches deployed at:", signedBatches.target);
+  // console.log("---");
+  // console.log("SlotsLogic deployed at:", slotsLogic.target);
+  // console.log("Slots deployed at:", slots.target);
+  // console.log("---");
+  // console.log("ValidatorsLogic deployed at:", validatorscLogic.target);
+  // console.log("Validators deployed at:", validatorsc.target);
+  // console.log("---");
+  // console.log("BridgeLogic owner:", await bridgeLogic.owner());
+  // console.log("Bridge owner:", await bridge.owner());
+  // console.log("---");
+  // console.log("ClaimsLogic maxNumberOfTransactions:", await claimsLogic.maxNumberOfTransactions());
+  // console.log("Claims maxNumberOfTransactions:", await claims.maxNumberOfTransactions());
+  // console.log("ClaimsLogic timeoutBlocksNumber:", await claimsLogic.timeoutBlocksNumber());
+  // console.log("Claims timeoutBlocksNumber:", await claims.timeoutBlocksNumber());
+  // console.log("---");
+  // console.log("ValdatorsLogic numberOfValidators:", await validatorscLogic.validatorsCount());
+  // console.log("Valdators numberOfValidators:", await validatorsc.validatorsCount());
+  // console.log("---");
+  // console.log("Validators chain data", await bridge.getValidatorsChainData(chain.id));
 
   // Proxy Bridge upgrade test
   // const BridgeV2 = await ethers.getContractFactory("BridgeV2");
