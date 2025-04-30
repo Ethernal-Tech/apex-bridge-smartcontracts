a70f51851fc8e6f27a9e145904ae1625ea4b01f5
MiroslavStefanovic <mstef@uns.ac.rs>
2025-04-30 18:27:07 +0200

    refactoring, added expect

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,4 +1,5 @@
 const { ethers, JsonRpcProvider } = require("ethers");
+const { expect } = require("chai");
 const config = require("./config.json");
 const adminJson = require("../artifacts/contracts/Admin.sol/Admin.json");
 const adminV2Json = require("../artifacts/contracts/AdminV2.sol/AdminV2.json");
@@ -122,9 +123,8 @@ async function main() {
   console.log(`Validators Proxy contract deployed at: ${validatorsProxyContract.target}`);
 
   console.log("--- Upgrading smart contracts");
-  // Proxy upgrade test
-  // Should create a copy of Bridge -> BridgeV2 smart contract with new version number
 
+  // Admin
   const proxyAdmin = new ethers.Contract(adminProxyContract.target, bridgeJson.abi, owner);
   const proxyBridge = new ethers.Contract(bridgeProxyContract.target, bridgeJson.abi, owner);
   const proxyClaims = new ethers.Contract(claimsProxyContract.target, claimsJson.abi, owner);
@@ -135,6 +135,7 @@ async function main() {
 
   let versionBefore = await proxyAdmin.version();
   console.log("Admin version before upgrade", versionBefore);
+  expect(versionBefore).to.equal("1.0.0");
   let implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   let storage = await provider.getStorage(proxyAdmin, implSlot);
   let implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
@@ -150,17 +151,19 @@ async function main() {
   let upgradeTx = await proxyAdmin.connect(owner).upgradeTo(newImplementationAddress);
   await upgradeTx.wait();
 
-  const proxyAdmin2 = new ethers.Contract(adminProxyContract.target, adminV2Json.abi, owner);
-  let versionAfter = await proxyAdmin2.version();
+  const proxyAdminV2 = new ethers.Contract(adminProxyContract.target, adminV2Json.abi, owner);
+  let versionAfter = await proxyAdminV2.version();
   console.log("Admin version after upgrade", versionAfter);
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-  storage = await provider.getStorage(proxyAdmin2, implSlot);
+  storage = await provider.getStorage(proxyAdminV2, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
   console.log("Admin implementation address after", implementationAddress);
+  expect(versionAfter).to.equal("1.0.1");
 
-  ///
+  // Bridge
   versionBefore = await proxyBridge.version();
   console.log("Bridge version before upgrade", versionBefore);
+  expect(versionBefore).to.equal("1.0.0");
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   storage = await provider.getStorage(proxyBridge, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
@@ -176,18 +179,19 @@ async function main() {
   upgradeTx = await proxyBridge.connect(owner).upgradeTo(newImplementationAddress);
   await upgradeTx.wait();
 
-  const proxyBridge2 = new ethers.Contract(bridgeProxyContract.target, bridgeV2Json.abi, owner);
-  versionAfter = await proxyBridge2.version();
+  const proxyBridgeV2 = new ethers.Contract(bridgeProxyContract.target, bridgeV2Json.abi, owner);
+  versionAfter = await proxyBridgeV2.version();
   console.log("Bridge version after upgrade", versionAfter);
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-  storage = await provider.getStorage(proxyBridge2, implSlot);
+  storage = await provider.getStorage(proxyBridgeV2, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
   console.log("Bridge implementation address after", implementationAddress);
+  expect(versionAfter).to.equal("1.0.1");
 
-  ////
-
+  // Claims
   versionBefore = await proxyClaims.version();
   console.log("Claims version before upgrade", versionBefore);
+  expect(versionBefore).to.equal("1.0.0");
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   storage = await provider.getStorage(proxyClaims, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
@@ -203,18 +207,19 @@ async function main() {
   upgradeTx = await proxyClaims.connect(owner).upgradeTo(newImplementationAddress);
   await upgradeTx.wait();
 
-  const proxyClaims2 = new ethers.Contract(claimsProxyContract.target, claimsV2Json.abi, owner);
-  versionAfter = await proxyClaims2.version();
+  const proxyClaimsV2 = new ethers.Contract(claimsProxyContract.target, claimsV2Json.abi, owner);
+  versionAfter = await proxyClaimsV2.version();
   console.log("Claims version after upgrade", versionAfter);
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-  storage = await provider.getStorage(proxyClaims2, implSlot);
+  storage = await provider.getStorage(proxyClaimsV2, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
   console.log("Claims implementation address after", implementationAddress);
+  expect(versionAfter).to.equal("1.0.1");
 
-  ////
-
+  // ClaimsHelper
   versionBefore = await proxyClaimsHelper.version();
   console.log("ClaimsHelper version before upgrade", versionBefore);
+  expect(versionBefore).to.equal("1.0.0");
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   storage = await provider.getStorage(proxyClaimsHelper, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
@@ -230,18 +235,19 @@ async function main() {
   upgradeTx = await proxyClaimsHelper.connect(owner).upgradeTo(newImplementationAddress);
   await upgradeTx.wait();
 
-  const proxyClaimsHelper2 = new ethers.Contract(claimsHelperProxyContract.target, claimsHelperV2Json.abi, owner);
-  versionAfter = await proxyClaimsHelper2.version();
+  const proxyClaimsHelperV2 = new ethers.Contract(claimsHelperProxyContract.target, claimsHelperV2Json.abi, owner);
+  versionAfter = await proxyClaimsHelperV2.version();
   console.log("ClaimsHelper version after upgrade", versionAfter);
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-  storage = await provider.getStorage(proxyClaimsHelper2, implSlot);
+  storage = await provider.getStorage(proxyClaimsHelperV2, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
   console.log("ClaimsHelper implementation address after", implementationAddress);
+  expect(versionAfter).to.equal("1.0.1");
 
-  ////
-
+  // SignedBatches
   versionBefore = await proxySignedBatches.version();
   console.log("SignedBatches version before upgrade", versionBefore);
+  expect(versionBefore).to.equal("1.0.0");
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   storage = await provider.getStorage(proxySignedBatches, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
@@ -261,18 +267,19 @@ async function main() {
   upgradeTx = await proxySignedBatches.connect(owner).upgradeTo(newImplementationAddress);
   await upgradeTx.wait();
 
-  const proxySignedBatches2 = new ethers.Contract(signedBatchesProxyContract.target, signedBatchesV2Json.abi, owner);
-  versionAfter = await proxySignedBatches2.version();
+  const proxySignedBatchesV2 = new ethers.Contract(signedBatchesProxyContract.target, signedBatchesV2Json.abi, owner);
+  versionAfter = await proxySignedBatchesV2.version();
   console.log("SignedBatches version after upgrade", versionAfter);
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-  storage = await provider.getStorage(proxySignedBatches2, implSlot);
+  storage = await provider.getStorage(proxySignedBatchesV2, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
   console.log("SignedBatches implementation address after", implementationAddress);
+  expect(versionAfter).to.equal("1.0.1");
 
-  ////
-
+  // Slots
   versionBefore = await proxySlots.version();
   console.log("Slots version before upgrade", versionBefore);
+  expect(versionBefore).to.equal("1.0.0");
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   storage = await provider.getStorage(proxySlots, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
@@ -288,18 +295,19 @@ async function main() {
   upgradeTx = await proxySlots.connect(owner).upgradeTo(newImplementationAddress);
   await upgradeTx.wait();
 
-  const proxySlots2 = new ethers.Contract(slotsProxyContract.target, slotsV2Json.abi, owner);
-  versionAfter = await proxySlots.version();
+  const proxySlotsV2 = new ethers.Contract(slotsProxyContract.target, slotsV2Json.abi, owner);
+  versionAfter = await proxySlotsV2.version();
   console.log("Slots version after upgrade", versionAfter);
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-  storage = await provider.getStorage(proxySlots2, implSlot);
+  storage = await provider.getStorage(proxySlotsV2, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
   console.log("Slots implementation address after", implementationAddress);
+  expect(versionAfter).to.equal("1.0.1");
 
-  ////
-
+  // Validators
   versionBefore = await proxyValidators.version();
   console.log("Validators version before upgrade", versionBefore);
+  expect(versionBefore).to.equal("1.0.0");
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
   storage = await provider.getStorage(proxyValidators, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
@@ -315,13 +323,14 @@ async function main() {
   upgradeTx = await proxyValidators.connect(owner).upgradeTo(newImplementationAddress);
   await upgradeTx.wait();
 
-  const proxyValidators2 = new ethers.Contract(validatorsProxyContract.target, validatorsV2Json.abi, owner);
-  versionAfter = await proxyValidators.version();
+  const proxyValidatorsV2 = new ethers.Contract(validatorsProxyContract.target, validatorsV2Json.abi, owner);
+  versionAfter = await proxyValidatorsV2.version();
   console.log("Validators version after upgrade", versionAfter);
   implSlot = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-  storage = await provider.getStorage(proxyValidators2, implSlot);
+  storage = await provider.getStorage(proxyValidatorsV2, implSlot);
   implementationAddress = ethers.getAddress("0x" + storage.slice(-40));
   console.log("Validators implementation address after", implementationAddress);
+  expect(versionAfter).to.equal("1.0.1");
 }
 
 main();
