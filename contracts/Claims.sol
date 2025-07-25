75430e26306c006dbb77142f4d831167ea042335
Zoltan Babinski <babinskizoltan@gmail.com>
2025-07-25 16:45:38 +0200

    version

--- contracts/Claims.sol
+++ contracts/Claims.sol
@@ -879,7 +879,7 @@ contract Claims is IBridgeStructs, Utils, Initializable, OwnableUpgradeable, UUP
     /// @notice Returns the current version of the contract
     /// @return A semantic version string
     function version() public pure returns (string memory) {
-        return "1.1.0";
+        return "1.1.1";
     }
 
     modifier onlyBridge() {
