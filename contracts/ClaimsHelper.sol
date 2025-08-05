d379d5c005e6979947bb8fd25bef2cbf4022f926
Igor Crevar <crewce@gmail.com>
2025-08-05 11:46:52 +0200

    AD-784 Reactor Bridge SC - Not enough funds event even when quorum is… (#130)
    * AD-729 Reactor - BridgingFee for destination (#123)
    
    ---------
    
    Co-authored-by: MiroslavStefanovic <mstef@uns.ac.rs>
    
    * AD-784 Reactor Bridge SC - Not enough funds event even when quorum is… (#129)
    
    * AD-784 Reactor Bridge SC - Not enough funds event even when quorum is reached
    
    * Remove duplicate function and commented out code (#131)
    
    ---------
    
    Co-authored-by: MiroslavStefanovic <mstef@uns.ac.rs>

--- contracts/ClaimsHelper.sol
+++ contracts/ClaimsHelper.sol
@@ -100,6 +100,23 @@ contract ClaimsHelper is IBridgeStructs, Utils, Initializable, OwnableUpgradeabl
         currentBatchBlock[destinationChainId] = int256(block.number);
     }
 
+    /// @notice Update number of votes for specific hash if needed and returns true if update was executed
+    /// @dev Update number of votes for specific hash if needed and returns true if update was executed
+    /// @param _hash hash
+    /// @param _validatorIdx index of validator
+    function updateVote(bytes32 _hash, uint8 _validatorIdx) external onlySignedBatchesOrClaims returns (bool) {
+        uint256 _bitmapValue = bitmap[_hash];
+        uint256 _newBitmapValue = _bitmapValue | (1 << _validatorIdx);
+
+        if (_newBitmapValue == _bitmapValue) {
+            return false;
+        }
+
+        bitmap[_hash] = _newBitmapValue;
+
+        return true;
+    }
+
     /// @notice Registers a vote for a specific claim hash only if the voter hasn't already voted and quorum hasn't been reached.
     /// @dev Increments the vote count if conditions are met and returns whether the quorum is now reached.
     /// @param _validatorIdx The index of validator in the validator set.
@@ -166,7 +183,7 @@ contract ClaimsHelper is IBridgeStructs, Utils, Initializable, OwnableUpgradeabl
     /// @notice Returns the current version of the contract
     /// @return A semantic version string
     function version() public pure returns (string memory) {
-        return "1.1.0";
+        return "1.1.1";
     }
 
     modifier onlySignedBatchesOrClaims() {
