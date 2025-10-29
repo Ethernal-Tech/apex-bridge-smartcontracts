3c9460a760e178078f795da0f8a9c7ab8b6479ce
MiroslavStefanovic <mstef@uns.ac.rs>
2025-10-29 19:47:40 +0100

    start

--- contracts/interfaces/BatchTypesLib.sol
+++ contracts/interfaces/BatchTypesLib.sol
@@ -7,4 +7,5 @@ library BatchTypesLib {
     uint8 internal constant CONSOLIDATION = 1;
     uint8 internal constant VALIDATORSET = 2;
     uint8 internal constant VALIDATORSET_FINAL = 3;
+    uint8 internal constant COLORED_COIN = 4;
 }
