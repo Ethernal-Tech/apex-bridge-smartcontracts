1034576abd34f5d297eadcec3a7f4ae96b70d54a
Igor Crevar <crewce@gmail.com>
2025-11-19 13:32:55 +0100

    additional tests

---
+++ test/ValidatorsUpdate.tests.ts
@@ -0,0 +1,260 @@
+import { Validators } from "../typechain-types";
+
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("submitNewValidatorSet+validatorSetUpdated", function () {
+    let validatorsSC: any;
+    let validatorsChainDataPrime: any;
+    let validatorsChainDataVector: any;
+    let chains: any;
+    let validatorsAddresses: string[];
+    let validatorsAdditional: any;
+    let contractSigner: any;
+
+    const generateKey = function (indx: number, prefix: number): string[] {
+        return [
+            (BigInt(prefix + indx * 4)).toString(),
+            (BigInt(prefix + indx * 4) + 1n).toString(),
+            (BigInt(prefix + indx * 4) + 2n).toString(),
+            (BigInt(prefix + indx * 4) + 3n).toString(),
+        ];
+    }
+
+    beforeEach(async () => {
+        const [owner, validator1, validator2, validator3, validator4, validator5, validator6] = await ethers.getSigners();
+        const ValidatorscProxy = await ethers.getContractFactory("ERC1967Proxy");
+        const Validators = await ethers.getContractFactory("Validators");
+        const validatorscLogic = await Validators.deploy();
+        validatorsAdditional = validator6;
+
+        validatorsAddresses = [
+            validator1.address,
+            validator2.address,
+            validator3.address,
+            validator4.address,
+            validator5.address,
+        ];
+
+        const validatorsProxy = await ValidatorscProxy.deploy(
+            await validatorscLogic.getAddress(),
+            Validators.interface.encodeFunctionData("initialize", [owner.address, owner.address, validatorsAddresses])
+        );
+
+        validatorsSC = Validators.attach(validatorsProxy.target) as Validators;
+
+        chains = [
+            { id: 1, chainType: 0, addressMultisig: "", addressFeePayer: "" },
+            { id: 2, chainType: 0, addressMultisig: "", addressFeePayer: "" },
+        ];
+
+        validatorsChainDataPrime = validatorsAddresses.map((addr, index) => ({
+            addr: addr,
+            data: {
+                key: generateKey(index, 0),
+            },
+            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+        }));
+
+        validatorsChainDataVector = validatorsAddresses.map((addr, index) => ({
+            addr: addr,
+            data: {
+                key: generateKey(index, 256),
+            },
+            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdee",
+            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567891",
+        }));
+
+        const fakeBridgeAddr = await validatorscLogic.getAddress();
+        //Impersonate the factory to make calls
+        contractSigner = await ethers.getSigner(fakeBridgeAddr);
+
+        //To make any call with impersonated, you will need to send some eth
+        const hre = require("hardhat");
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [contractSigner.address],
+        });
+        await ethers.provider.send("hardhat_setBalance", [contractSigner.address, "0x56BC75E2D63100000"]);
+
+        await validatorsSC.connect(owner).setDependencies(fakeBridgeAddr);
+        await validatorsSC.connect(contractSigner).setValidatorsChainData(1, validatorsChainDataPrime);
+        await validatorsSC.connect(contractSigner).setValidatorsChainData(2, validatorsChainDataVector);
+    });
+
+    it("add one, removes on index 1 and 3, two chains", async () => {
+        const newValidatorSetDelta = {
+            addedValidators: [
+                {
+                    chainId: 1,
+                    validators: [
+                        {
+                            addr: validatorsAdditional.address,
+                            data: {
+                                key: generateKey(6, 0),
+                            },
+                            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+                            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+                        },
+                    ],
+                },
+                {
+                    chainId: 2,
+                    validators: [
+                        {
+                            addr: validatorsAdditional.address,
+                            data: {
+                                key: generateKey(6, 256),
+                            },
+                            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+                            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+                        },
+                    ],
+                },
+                {
+                    chainId: 255,
+                    validators: [
+                        {
+                            addr: validatorsAdditional.address,
+                            data: {
+                                key: generateKey(6, 65535),
+                            },
+                            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+                            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+                        },
+                    ],
+                },
+            ],
+            removedValidators: [validatorsAddresses[1], validatorsAddresses[3]],
+        }
+
+        expect(await validatorsSC.validatorsCount()).to.equal(5);
+
+        const chainDataPrimeOld = await validatorsSC.getValidatorsChainData(1);
+        const chainDataVectorOld = await validatorsSC.getValidatorsChainData(2);
+
+        await validatorsSC.connect(contractSigner).submitNewValidatorSet(newValidatorSetDelta, chains);
+        await validatorsSC.connect(contractSigner).validatorSetUpdated(chains);
+
+        const chainDataPrime = await validatorsSC.getValidatorsChainData(1);
+        const chainDataVector = await validatorsSC.getValidatorsChainData(2);
+
+        expect(await validatorsSC.validatorsCount()).to.equal(4);
+        expect(chainDataPrime.length).to.equal(4);
+        expect(chainDataVector.length).to.equal(4);
+
+        let indx = 0
+        for (let i = 0; i < chainDataPrime.length; i++) {
+            const prime = chainDataPrime[i];
+            const vector = chainDataVector[i];
+
+            if (i != chainDataPrime.length - 1) {
+                expect(prime.key).to.deep.equal(chainDataPrimeOld[indx].key);
+                expect(vector.key).to.deep.equal(chainDataVectorOld[indx].key);
+            } else {
+                expect(prime.key).to.deep.equal(generateKey(6, 0));
+                expect(vector.key).to.deep.equal(generateKey(6, 256));
+            }
+
+            indx += 1 + (i == 0 || i == 1 ? 1 : 0);
+        }
+    });
+
+    it("cumulative", async () => {
+        const newValidatorSetDelta = {
+            addedValidators: [
+                {
+                    chainId: 1,
+                    validators: [
+                        {
+                            addr: validatorsAdditional.address,
+                            data: {
+                                key: generateKey(6, 0),
+                            },
+                            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+                            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+                        },
+                    ],
+                },
+                {
+                    chainId: 2,
+                    validators: [
+                        {
+                            addr: validatorsAdditional.address,
+                            data: {
+                                key: generateKey(6, 256),
+                            },
+                            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+                            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+                        },
+                    ],
+                },
+                {
+                    chainId: 255,
+                    validators: [
+                        {
+                            addr: validatorsAdditional.address,
+                            data: {
+                                key: generateKey(6, 65535),
+                            },
+                            keySignature: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+                            keyFeeSignature: "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+                        },
+                    ],
+                },
+            ],
+            removedValidators: [validatorsAddresses[4]],
+        }
+
+        expect(await validatorsSC.validatorsCount()).to.equal(5);
+
+        const chainDataPrimeOld = await validatorsSC.getValidatorsChainData(1);
+        const chainDataVectorOld = await validatorsSC.getValidatorsChainData(2);
+
+        await validatorsSC.connect(contractSigner).submitNewValidatorSet(newValidatorSetDelta, chains);
+        await validatorsSC.connect(contractSigner).validatorSetUpdated(chains);
+
+        let chainDataPrime = await validatorsSC.getValidatorsChainData(1);
+        let chainDataVector = await validatorsSC.getValidatorsChainData(2);
+
+        expect(await validatorsSC.validatorsCount()).to.equal(5);
+        expect(chainDataPrime.length).to.equal(5);
+        expect(chainDataVector.length).to.equal(5);
+
+        for (let i = 0; i < chainDataPrime.length; i++) {
+            const prime = chainDataPrime[i];
+            const vector = chainDataVector[i];
+
+            if (i != chainDataPrime.length - 1) {
+                expect(prime.key).to.deep.equal(chainDataPrimeOld[i].key);
+                expect(vector.key).to.deep.equal(chainDataVectorOld[i].key);
+            } else {
+                expect(prime.key).to.deep.equal(generateKey(6, 0));
+                expect(vector.key).to.deep.equal(generateKey(6, 256));
+            }
+        }
+
+        // now again delete last one, the one we previosly added
+        await validatorsSC.connect(contractSigner).submitNewValidatorSet({
+            addedValidators: [],
+            removedValidators: [validatorsAdditional.address],
+        }, chains);
+        await validatorsSC.connect(contractSigner).validatorSetUpdated(chains);
+
+        chainDataPrime = await validatorsSC.getValidatorsChainData(1);
+        chainDataVector = await validatorsSC.getValidatorsChainData(2);
+
+        expect(await validatorsSC.validatorsCount()).to.equal(4);
+        expect(chainDataPrime.length).to.equal(4);
+        expect(chainDataVector.length).to.equal(4);
+
+        for (let i = 0; i < chainDataPrime.length; i++) {
+            const prime = chainDataPrime[i];
+            const vector = chainDataVector[i];
+
+            expect(prime.key).to.deep.equal(chainDataPrimeOld[i].key);
+            expect(vector.key).to.deep.equal(chainDataVectorOld[i].key);
+        }
+    });
+});
