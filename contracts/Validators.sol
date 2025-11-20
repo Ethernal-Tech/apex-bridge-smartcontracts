bc4488a06d06146247340a3955ceaeb7fad623c8
Igor Crevar <crewce@gmail.com>
2025-11-20 13:00:08 +0100

    versions

--- contracts/Validators.sol
+++ contracts/Validators.sol
@@ -664,7 +664,7 @@ contract Validators is IBridgeStructs, Utils, Initializable, OwnableUpgradeable,
     /// @notice Returns the current version of the contract
     /// @return A semantic version string
     function version() public pure returns (string memory) {
-        return "1.1.0";
+        return "1.1.1";
     }
 
     modifier onlyBridge() {
